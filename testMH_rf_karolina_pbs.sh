d25bae1749ec6a909cf28880985d757512ee8174
hrbam <101473937+hrbam@users.noreply.github.com>
2022-03-23 15:26:32 +0100

    Update testMH_rf_karolina_pbs.sh

--- testMH_rf_karolina_pbs.sh
+++ testMH_rf_karolina_pbs.sh
@@ -11,9 +11,14 @@ pwd
 module load R
 echo "loaded R"
 
+#time Rscript rf_cv_serial.r
+time Rscript rf_cv_mc.r 8
+time Rscript rf_cv_mc.r 16
+time Rscript rf_cv_mc.r 32
+#time Rscript rf_cv_mc.r 64
+#time Rscript rf_cv_mc.r 128
 
-
-time Rscript rf_mc.r 16
-time Rscript rf_mc.r 32
-time Rscript rf_mc.r 64
-time Rscript rf_mc.r 128
+#time Rscript rf_mc.r 16
+#time Rscript rf_mc.r 32
+#time Rscript rf_mc.r 64
+#time Rscript rf_mc.r 128
